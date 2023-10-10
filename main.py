1edcdd53a8c2d110d08e4408008210faf7c50145
njp666 <147472574+njp666@users.noreply.github.com>
2023-10-10 06:05:43 +0000

    added

---
+++ main.py
@@ -0,0 +1,7 @@
+# import platform module to grab the current user's machine name...
+import platform as plat
+
+host = plat.node
+
+#print statement to console...
+print("Your current hostname is: " + host)
