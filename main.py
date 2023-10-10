fc1532126675ba6bbc7e4184058bdfbf4c839983
njp666 <147472574+njp666@users.noreply.github.com>
2023-10-10 06:07:16 +0000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+"""abcd"""
 # import platform module to grab the current user's machine name...
-import platform as plat
-
-host = plat.node
+from platform import node as hostname
+import sys
 
 #print statement to console...
-print("Your current hostname is: " + host)
+print("Your current hostname is: " + hostname())
+
+print("Enter your option below: \n" +
+      "(1) Check your new hostname against NetBIOS rules \n"
+       + "or \n" + "(2) Exit \n")
+userInput = input("Option: ")
+
+if userInput == str("1"):
+    print("1")
+    sys.exit()
+else:
+    sys.exit()
