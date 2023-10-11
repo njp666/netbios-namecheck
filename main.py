1067012ff33238a712cab6bbb2d987bae4e68ac3
unknown <Nick@ORION.vxresearch.org>
2023-10-11 11:16:27 +1100

    rewritten - added better menu functionality

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 """abcd"""
 # import platform module to grab the current user's machine name...
 from platform import node as hostname
-import sys
+host = hostname()
 
-#print statement to console...
-print("Your current hostname is: " + hostname())
+def mainMenu():
+    while (True):
+        print("Enter your menu option:"+
+          "\n(1) Check current hostname"+
+          "\n(2) Check proposed hostname length"+
+          "\n(3) Exit")
+        userInput = int(input("\nOption: "))
 
-print("Enter your option below: \n" +
-      "(1) Check your new hostname against NetBIOS rules \n"
-       + "or \n" + "(2) Exit \n")
-userInput = input("Option: ")
+#prints the current machine hostname and gives length.
+        if userInput == 1:
+            print("Your current hostname is:\n"+hostname()+
+                  "\nHostname length: "
+                  +str(len(host))+"\n")
+        
+        if userInput == 2:
+            print("\nCurrent hostname: "+host)
+            userHost = input("Enter your desired hostname: ")
+            if len(userHost) > 15:
+                print("\nHostname too long! Length is: "+str(len(userHost))+"\nMax: 15 characters.\n")
+            
+            if len(userHost) <= 15:
+                print("Length OK.\nYour hostname is: "+userHost+". \nLength: "+str(len(userHost))+"\n")
 
-if userInput == str("1"):
-    print("1")
-    sys.exit()
-else:
-    sys.exit()
+        if userInput == 3:
+            print("Exiting...\n")
+            quit()
+mainMenu()
