e78acdaf66ea130ca181b5ecbc867091407ac273
Nicholas Piet <nicholas.piet33@gmail.com>
2023-10-11 12:04:40 +1100

    added zero-character condition
    	modified:   main.py

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ def mainMenu():
             print("Your current hostname is:\n"+host+
                   "\nHostname length: "
                   +str(len(host))+"\n")
-        
+
+#asks the user for input and         
         if userInput == 2:
             print("\nCurrent hostname: "+host)
-            userHost = input("Enter your desired hostname: ")
-            if len(userHost) > 15:
-                print("\nHostname too long! Length is: "+str(len(userHost))+"\nMax: 15 characters.\n")
+            userHost = input("Enter your desired hostname (This is NOT the FQDN, e.g. hostname.foo.bar): ")
+           
+            if len(userHost) >= 1 and len(userHost) <= 15:
+                print("Length OK.\nYour hostname is: "+userHost+"\nLength: "+str(len(userHost))+"\n")
             
-            if len(userHost) <= 15:
-                print("Length OK.\nYour hostname is: "+userHost+". \nLength: "+str(len(userHost))+"\n")
+            elif len(userHost) <= 0:
+                 print("Error. Hostname must be greater than 0 characters.")
+
+            else:
+                print("\nHostname too long! Length is: "+str(len(userHost))+"\nMax: 15 characters.\n")
 
         if userInput == 3:
             print("Exiting...\n")
